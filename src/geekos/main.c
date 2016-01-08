aed8f61e9064dac9e712a5c7bb29fce187e09cbe
sinabeuro <ican312@hanmail.net>
2016-01-09 01:06:55 +0900

    first commit

---
+++ src/geekos/main.c
@@ -0,0 +1,85 @@
+/*
+ * GeekOS C code entry point
+ * Copyright (c) 2001,2003,2004 David H. Hovemeyer <daveho@cs.umd.edu>
+ * Copyright (c) 2003, Jeffrey K. Hollingsworth <hollings@cs.umd.edu>
+ * Copyright (c) 2004, Iulian Neamtiu <neamtiu@cs.umd.edu>
+ * $Revision: 1.51 $
+ * 
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "COPYING".
+ */
+
+#include <geekos/bootinfo.h>
+#include <geekos/string.h>
+#include <geekos/screen.h>
+#include <geekos/mem.h>
+#include <geekos/crc32.h>
+#include <geekos/tss.h>
+#include <geekos/int.h>
+#include <geekos/kthread.h>
+#include <geekos/trap.h>
+#include <geekos/timer.h>
+#include <geekos/keyboard.h>
+
+
+void startFunc()
+{
+	Keycode keycode;
+	Print("Hello from nemesis\n");
+	while(true)
+	{
+		keycode = Wait_For_Key();
+		Print("%c\n", keycode);
+
+		if(keycode == 0x4064)
+			break;
+	}
+}
+
+
+/*
+ * Kernel C code entry point.
+ * Initializes kernel subsystems, mounts filesystems,
+ * and spawns init process.
+ */
+void Main(struct Boot_Info* bootInfo)
+{
+	struct Kernel_Thread *kthread;
+
+    Init_BSS();
+    Init_Screen();
+    Init_Mem(bootInfo);
+    Init_CRC32();
+    Init_TSS();
+    Init_Interrupts();
+    Init_Scheduler();
+    Init_Traps();
+    Init_Timer();
+    Init_Keyboard();
+
+
+    Set_Current_Attr(ATTRIB(BLACK, GREEN|BRIGHT));
+    Print("Welcome to GeekOS!\n");
+    Set_Current_Attr(ATTRIB(BLACK, GRAY));
+
+
+	kthread = Start_Kernel_Thread(startFunc, 0, PRIORITY_NORMAL, 0);
+    //TODO("Start a kernel thread to echo pressed keys and print counts");
+
+
+	Join(kthread);
+    //Print("Joined!\n");
+
+
+    /* Now this thread is done. */ 
+    Exit(0);
+}
+
+
+
+
+
+
+
+
+
